cbf5fd312c0f05862df529073bb4c453c0f09497
Jeff "Hawke" Ravatt <hawke@unrestrainedgames.com>
2017-04-06 13:21:21 -0700

    Completed all the includes...

--- server.js
+++ server.js
@@ -1,14 +1,54 @@
 // Load the express module (Where do you think this comes from?)
 var express = require("express");
-var path = require("path")
+// require path for filename normaliziation.
+var path = require("path");
+// require body-parser
+var bodyParser = require('body-parser');
+
+//enable if you want to use Session (Make sure you install express-session):
+// var session = require('express-session');
+
 var PORT = 8000;
+
 // invoke var express and store the resulting application in var app
 var app = express();
 
-// let's handle the base route "/" and respond with "Hello Express"
+//also enable if you want to use Session:
+// app.use(session({secret: 'codingdojorocks'}));  // string for encryption
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(path.join(__dirname, "./static"))); // two underscores before dirname,
+// this sets up the views path for EJS files.
+app.set('views', path.join(__dirname, './static/views'));
+
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+// Middleware to convert POST data to JSON (req.body...)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ********* Begin Controllers ***************
+
 app.get('/', function(request, response) {
-    response.send("<h1>Hello Express</h1>");
+    response.render('main');
 })
+
+app.get("/users", function(request, response) {
+    // hard-coded user data
+    var users_array = [
+        { name: "Michael", email: "michael@codingdojo.com" },
+        { name: "Jay", email: "jay@codingdojo.com" },
+        { name: "Brendan", email: "brendan@codingdojo.com" },
+        { name: "Andrew", email: "andrew@codingdojo.com" }
+    ];
+    response.render('index', { users: users_array });
+})
+
+app.post("/users", function(request, response) {
+        console.log("POST DATA \n\n", request.body);
+        response.redirect("/");
+    })
+    // ******** End Controllers *****************
 
 // Tell the express app to listen on port PORT
 app.listen(PORT, function() {
